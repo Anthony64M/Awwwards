9fc739c6e19aa60f938449998ce1168ae302acd5
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-10-26 16:19:52 +0300

    Added requirements

--- awwards/views.py
+++ awwards/views.py
@@ -17,18 +17,12 @@ def index(request):
             post = form.save(commit=False)
             post.user = request.user
             post.save()
+
+
     else:
         form = PostForm()
-
-    try:
-        posts = Post.objects.all()
-        posts = posts[::-1]
-        a_post = random.randint(0, len(posts)-1)
-        random_post = posts[a_post]
-        print(random_post.photo)
-    except Post.DoesNotExist:
-        posts = None
-    return render(request, 'index.html', {'posts': posts, 'form': form, 'random_post': random_post})
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'form': form,'posts': posts})
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
